fc7d7bf9a9982215e756b2abc317e1a0fd2435f5
senaygui <senaygeletaw@gmail.com>
2025-11-20 11:25:35 +0300

    update

--- app/admin/course_registrations.rb
+++ app/admin/course_registrations.rb
@@ -1,28 +1,28 @@
 ActiveAdmin.register CourseRegistration do
-  menu parent: "Student managment"
+  menu parent: 'Student managment'
   config.batch_actions = true
   permit_params :course_section, :enrollment_status, :course_section_id
 
   active_admin_import validate: true,
                       csv_options: { col_sep: ',' },
                       headers_rewrites: {
-                        :'Student ID' => :student_id,
-                        :'Program ID' => :program_id,
-                        :'Semester Registration ID' => :semester_registration_id,
-                        :'Department ID' => :department_id,
-                        :'Course ID' => :course_id,
-                        :'Academic Calendar ID' => :academic_calendar_id,
-                        :'Section ID' => :section_id,
-                        :'Semester' => :semester,
-                        :'Year' => :year,
-                        :'Student Full Name' => :student_full_name,
-                        :'Enrollment Status' => :enrollment_status,
-                        :'Course Title' => :course_title,
-                        :'Academic Year' => :academic_year,
-                        :'Drop Pending Status' => :drop_pending_status,
-                        :'Is Active' => :is_active
+                        'Student ID': :student_id,
+                        'Program ID': :program_id,
+                        'Semester Registration ID': :semester_registration_id,
+                        'Department ID': :department_id,
+                        'Course ID': :course_id,
+                        'Academic Calendar ID': :academic_calendar_id,
+                        'Section ID': :section_id,
+                        'Semester': :semester,
+                        'Year': :year,
+                        'Student Full Name': :student_full_name,
+                        'Enrollment Status': :enrollment_status,
+                        'Course Title': :course_title,
+                        'Academic Year': :academic_year,
+                        'Drop Pending Status': :drop_pending_status,
+                        'Is Active': :is_active
                       },
-                      before_import: ->(importer) {
+                      before_import: lambda { |importer|
                         # Access the CSV lines
                         csv_lines = importer.instance_variable_get(:@csv_lines)
                         headers = importer.instance_variable_get(:@headers)
@@ -30,12 +30,12 @@ ActiveAdmin.register CourseRegistration do
                         puts "Headers: #{headers.inspect}"
 
                         if csv_lines.nil? || csv_lines.empty?
-                          puts "Warning: csv_lines is nil or empty"
+                          puts 'Warning: csv_lines is nil or empty'
                         else
                           # Check the structure of the first CSV line
                           first_line = csv_lines.first
                           puts "First CSV line: #{first_line.inspect}"
-                          
+
                           # Determine how to extract the IDs
                           id_key = headers.key('id') || 'id'
                           ids = csv_lines.map { |row| row[id_key] }.compact
@@ -43,7 +43,7 @@ ActiveAdmin.register CourseRegistration do
                           CourseRegistration.where(id: ids).delete_all
                         end
                       },
-                      after_import: ->(importer) {
+                      after_import: lambda { |importer|
                         # Access the CSV lines
                         csv_lines = importer.instance_variable_get(:@csv_lines)
                         headers = importer.instance_variable_get(:@headers)
@@ -51,25 +51,25 @@ ActiveAdmin.register CourseRegistration do
                         id_key = headers.key('id') || 'id'
                         imported_ids = csv_lines.map { |row| row[id_key] }.compact
                         puts "IDs imported: #{imported_ids.inspect}"
-                        
+
                         CourseRegistration.where(id: imported_ids).update_all(updated_at: Time.zone.now)
                       }
 
-  scoped_collection_action :scoped_collection_update, title: 'Set Section', form: -> do
+  scoped_collection_action :scoped_collection_update, title: 'Set Section', form: lambda {
     { section_id: Section.all.map { |section| [section.section_full_name, section.id] } }
-  end
+  }
 
   controller do
     def scoped_collection
-      super.where("enrollment_status = ?", "enrolled")
+      super.where('enrollment_status = ?', 'enrolled')
     end
   end
 
-  batch_action "Generate Grade Sheet", method: :put, confirm: "Are you sure?" do |ids|
+  batch_action 'Generate Grade Sheet', method: :put, confirm: 'Are you sure?' do |ids|
     CourseRegistration.find(ids).each do |course_registration|
       course_registration.add_grade
     end
-    redirect_to collection_path, notice: "Grade Sheet Is Generated Successfully"
+    redirect_to collection_path, notice: 'Grade Sheet Is Generated Successfully'
   end
   filter :student_id, as: :search_select_filter, url: proc { admin_students_path },
                       fields: %i[student_id id], display_name: 'student_id', minimum_input_length: 2,
@@ -91,15 +91,15 @@ ActiveAdmin.register CourseRegistration do
   filter :semester
   filter :year
   filter :section_id, as: :search_select_filter, url: proc { admin_course_sections_path },
-                             fields: %i[section_full_name id], display_name: 'section_full_name', minimum_input_length: 2,
-                             order_by: 'id_asc'
+                      fields: %i[section_full_name id], display_name: 'section_full_name', minimum_input_length: 2,
+                      order_by: 'id_asc'
   filter :enrollment_status
   index do
     selectable_column
-    column "Student Name" do |s|
+    column 'Student Name' do |s|
       s.student.name.full
     end
-    column "Academic Year" do |s|
+    column 'Academic Year' do |s|
       s.get_academic_year
     end
     column :id do |c|
@@ -112,9 +112,22 @@ ActiveAdmin.register CourseRegistration do
     column :section_name do |c|
       c.section.section_short_name if c.section.present?
     end
-    column "Created At", sortable: true do |c|
-      c.created_at.strftime("%b %d, %Y")
+    column 'Created At', sortable: true do |c|
+      c.created_at.strftime('%b %d, %Y')
     end
     actions
+  end
+
+  csv do
+    column('Student Name') { |s| s.student.name.full }
+    column('Academic Year') { |s| s.get_academic_year }
+    column('Student ID') { |c| c.student.student_id }
+    column(:course_title)
+    column('Program') { |c| c.program.program_name }
+    column('Section') { |c| c.section.section_short_name if c.section.present? }
+    column(:semester)
+    column(:year)
+    column(:enrollment_status)
+    column('Created At') { |c| c.created_at.strftime('%b %d, %Y') }
   end
 end
